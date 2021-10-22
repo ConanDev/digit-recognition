f7021c43444131094fa945a3cda2513496c67a59
Conan <59874981+ConanDev@users.noreply.github.com>
2021-10-22 21:56:41 +0300

    Complete App Colored box margins Code cleanup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import * as tf from '@tensorflow/tfjs'
 import {useRef, useState, useEffect} from 'react'
 import SignaturePad from 'react-signature-canvas'
+import {Bar} from 'react-chartjs-2'
 
+//Digit-recognition model path, which needs to be fetched from a server
 const modelPath = 'http://localhost:8080/model.json'
 let model
+
+const options = {
+  scales: {
+      xAxes: [{
+          barPercentage: 0.4
+      }]
+  }
+}
 
 async function loadModel () {
   model = await tf.loadLayersModel(modelPath)
 }
-
-
 
  function App() {
   //load the digit-recognition ML model
@@ -21,39 +29,38 @@ async function loadModel () {
     }
   }, [])
 
-  let sigRef = useRef({})
+  //reference for the drawing pad
+  let padRef = useRef({})
   const [prediction, setPrediction] = useState("None")
+  const [hist, setHist] = useState(null)
   const clearBoard = () => {
-    sigRef.clear()
+    padRef.clear()
+    setHist(null)
   }
 
   async function Predict () {
-    // const canvas = sigRef.getTrimmedCanvas()
-    const canvas = sigRef.getCanvas()
+    const canvas = padRef.getCanvas()
     const tensor = preprocessCanvas(canvas)
-    // const model = await tf.loadLayersModel(modelPath)
-    console.log(Object.keys(model))
-
+    //predData.dataSync() == array of confidences/predicted probabilites for each number
     const predData = model.predict(tensor)
+    const histogramData = {
+      labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      datasets: [
+        {
+          label: 'Confidence',
+          backgroundColor: '#EC932F',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: predData.dataSync()
+        }
+      ]
+    }
+    setHist(histogramData)
+    //actual prediction i.e. guessed number
     const pred = predData.argMax(1)
-
     setPrediction(pred.dataSync()[0])
-
-    // .then((actualModel)=>{
-    //   console.log("success loading the model:\n")
-    //   console.log("attempting to predict:\n")
-    //   const predData = actualModel.predict(tensor)
-    //   const confidence = predData.max()
-    //   const pred = predData.argMax(1)
-    //   const keys = Object.keys(pred)
-    //   console.log("values:\n" + predData.values)
-    //   console.log("typeof pred: " + typeof(pred))
-    //   keys.forEach(key => {
-    //     console.log(`pred[${key}]: ${pred[key]}`)
-    //   })
-    //   setPrediction(pred.dataSync()[0])
-    // })
-    // .catch((err)=>{console.log(`Error:\n ${err}`)})
   }
 
   function preprocessCanvas(image) {
@@ -80,13 +87,26 @@ async function loadModel () {
       <h1>Hand-written digit recognizer</h1> <br/>
       <p>Draw a digit from 0 to 9 on the drawing pad.</p>
       <p> Click on "Predict" to predict it. Click on "Clear board" to clear the drawing pad.</p>
-      <SignaturePad ref={(ref)=>{sigRef=ref}} canvasProps={sigCanvasProps} 
-      backgroundColor="white" minWidth = {penThickness} maxWidth={penThickness}/>
+      <div style={styles.signaturePad}>
+        <SignaturePad ref={(ref)=>{padRef=ref}} canvasProps={sigCanvasProps} 
+        backgroundColor="white" minWidth = {penThickness} maxWidth={penThickness}/>
+      </div>
+      
       <br/><br/>
       <button onClick={clearBoard}>Clear board</button>
       <button onClick={Predict}>Predict</button>
       <h1>Prediction is {prediction}</h1>
+      <Bar data={hist} width={100} height={50} options={options}/>
     </div>
   );
 }
 export default App;
+
+const margin = 570
+const styles = {
+  signaturePad:{
+    backgroundColor: 'yellow',
+    marginLeft: margin,
+    marginRight: margin
+  }
+}
